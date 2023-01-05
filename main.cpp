afe9f0753e274bdeaca515284f87629f9ef98aef
murat-sever <murat-sever@live.com>
2023-01-05 21:11:55 +0000

    ch17 prelab1/2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <stack>
 
 using namespace std;
 
@@ -9,5 +11,24 @@ int main() {
     string data;
     getline(cin, data);
     cout << "Output: " << data << "\n\n";
+
+    ifstream file;
+    string str;
+    stack<string> myStack;
+    file.open("if.txt", ios_base::in);
+    cout << "pushing" << endl;
+    while(getline(file, str))
+    {
+        cout << str << endl;
+        myStack.push(str);
+    }
+    cout << "popping" << endl;
+    while (!myStack.empty())
+    {
+        cout << myStack.top() << endl;
+        myStack.pop();
+    }
+    
+    file.close();
     return 0;
 }
